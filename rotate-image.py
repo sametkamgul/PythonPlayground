da4b69a917823a8b3c7d0fc179bfc0e65ddae57e
sametmate <samet@mate.com.tr>
2019-06-25 15:38:04 +0300

    Qr code generation is added.

---
+++ rotate-image.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2 as cv
+
+def rotateImage(image, angle):
+  image_center = tuple(np.array(image.shape[1::-1]) / 2)
+  rot_mat = cv.getRotationMatrix2D(image_center, angle, 1.0)
+  result = cv.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv.INTER_LINEAR)
+  return result
+
+if __name__ == '__main__':
+	print('[INFO] app is started')
+	angle = 30 # in degree, counter-clockwise rotation
+	img = cv.imread('testImages/robot.jpg')
+	output = rotateImage(img, angle)
+	cv.imwrite('outputImages/rotated-image.png', output)
+	print('[INFO] job is done')
+
