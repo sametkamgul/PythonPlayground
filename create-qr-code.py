bf0996134b38b0019250b53fa5d88943876ef875
sametmate <samet@mate.com.tr>
2019-06-25 15:30:11 +0300

    Added additional codes and folder(s)/file(s)

---
+++ create-qr-code.py
@@ -0,0 +1,23 @@
+# Import QR Code library
+import qrcode
+
+# Create qr code instance
+qr = qrcode.QRCode(
+    version = 1,
+    error_correction = qrcode.constants.ERROR_CORRECT_H,
+    box_size = 10,
+    border = 4,
+)
+
+# The data that you want to store
+data = "https://sametkamgul.wordpress.com/"
+
+# Add data
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR Code instance
+img = qr.make_image()
+
+# Save it somewhere, change the extension as needed:
+img.save("image.jpg")
